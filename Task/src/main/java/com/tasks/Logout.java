0e87193f2580c32c2653d4277a4b34e94e9cc48c
debargooo <debarghyamessi1234@gmail.com>
2024-11-17 15:51:19 +0530

    changes

---
+++ Task/src/main/java/com/tasks/Logout.java
@@ -0,0 +1,59 @@
+package com.tasks;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Logout
+ */
+@WebServlet("/logout")
+public class Logout extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Logout() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		Cookie usernameCookie = new Cookie("username", null);
+		usernameCookie.setMaxAge(0); // Delete the cookie
+		response.addCookie(usernameCookie);
+
+		Cookie emailCookie = new Cookie("email", null);
+		emailCookie.setMaxAge(0); // Delete the cookie
+		response.addCookie(emailCookie);
+
+		// Invalidate session
+		HttpSession session = request.getSession();
+		session.invalidate();
+
+		// Redirect to login page
+		response.sendRedirect("/Login_Registration/login.jsp");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
+
